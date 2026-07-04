a2a1e4aaea7b51dfc2e2ec3abbc809fc0f8ea696
wharfman <gak2040@gmail.com>
2026-07-04 14:33:03 -0600

    Vocabe: text pre-processing pipeline created

--- train_model.py
+++ train_model.py
@@ -13,3 +13,80 @@ import clip
 from tqdm import tqdm
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+class Vocab:
+    '''
+    text pre-processing pipeline
+    convert words in sentences to nums for nn to read
+    '''
+    def __init__(self, min_freq=1, max_size=None):
+        self.min_freq = min_freq
+        self.max_size = max_size
+        self.word2indx = {}
+        self.indx2word = {}
+
+        # special tokens
+        self.PAD = '<PAD>' # accounts for empty spaces
+        self.SOS = '<SOS>' # start of sentence
+        self.EOS = '<EOS>' # end of sentence
+        self.UNK = '<UNK>' # replace unknown words with 'UNK'
+
+    def build_vocab(self, sentences):
+        """ 
+        tokenize all sentences and count num of times each word appears
+        """
+        counter = Counter()
+        for s in sentences:
+            tokens = self.tokenize(s)
+            counter.update(tokens)
+
+        # filter by min_freq
+        items = [(w, c) for w, c in counter.items() if c >= self.min_freq]
+        items.sort(key=lambda x: (-x[1], x[0]))
+        if self.max_size:
+            items = items[:self.max_size]
+
+        indx = 0
+        for sp in [self.PAD, self.SOS, self.EOS, self.UNK]:
+            self.word2indx[sp] = indx
+            self.indx2word[indx] = sp
+            indx +=1
+
+        for w, _ in items:
+            if w in self.word2indx:
+                continue
+            self.word2indx[w] = indx
+            self.indx2word[indx] = w
+            indx += 1
+
+    def tokenize(self, s):
+        # tokenizer: lowercase + split on spaces, strip punctuation
+        s = s.lower().strip()
+
+        # replace common punctuation with space
+        for char in [".", ",", "!", "?", ";", ":", '"', "'", "(", ")"]:
+            s = s.replace(char, ' ')
+
+        tokens = [t for t in s.split() if t]
+        return tokens
+    
+    def encode(self, s):
+        # convert sentence into list of nums
+        tokens = [self.SOS] + self.tokenize(s) + [self.EOS]
+        ids = [self.word2indx.get(t, self.word2indx[self.UNK]) for t in tokens]
+        return ids
+    
+    def decode(self, ids):
+        words = []
+        for i in ids:
+            w = self.indx2word.get(i, self.UNK)
+            if w == self.EOS:
+                break # stop due to end of sentence found
+            if w in (self.SOS, self.PAD):
+                continue # skip special tokens
+            words.append(w)
+        return ' '.join(words)
+    
+    def __len__(self):
+        return len(self.word2indx)
